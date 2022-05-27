315a0a04fd971d40cd301637482cd069d59ad92c
Shubham <shubham.prasad10101@gmail.com>
2022-05-27 15:54:27 +0530

    added css in public folder for homepage

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs")
+})
+
+app.listen(3000,()=>{
+    console.log("Server running at port 3000")
+})
