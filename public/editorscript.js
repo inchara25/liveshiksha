97c9468d064d1a1cc9cde05a767ae773d5d57c76
Shoaib Ahmed S <66957744+shoaibahmed9138@users.noreply.github.com>
2022-05-27 21:34:06 +0530

    asdf

--- public/editorscript.js
+++ public/editorscript.js
@@ -128,3 +128,12 @@ $("textarea").keydown(function (e) {
 });
 
 $("#source").focus();
+
+
+var QRCode = require('qrcode')
+var canvas = document.getElementById('canvas')
+
+QRCode.toCanvas(canvas, 'sample text', function (error) {
+  if (error) console.error(error)
+  console.log('success!');
+})
